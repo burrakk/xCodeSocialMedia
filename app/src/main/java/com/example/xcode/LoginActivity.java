fb50387e2bfd34af69fd6b529b2974598082a7b8
Burak Ikinci <burrakk54@gmail.com>
2020-11-29 00:09:47 +0300

    Google Sign In and couple changes to ui

--- app/src/main/java/com/example/xcode/LoginActivity.java
+++ app/src/main/java/com/example/xcode/LoginActivity.java
@@ -77,7 +77,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
