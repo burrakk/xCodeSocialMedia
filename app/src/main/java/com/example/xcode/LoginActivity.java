3cea218979740c28e82cfb17c0d2544b6698cc0e
Burak Ikinci <burrakk54@gmail.com>
2020-11-29 00:07:57 +0300

    First Commit of the Working Basics of the profile and the wall for all user posts with reg/log in

--- app/src/main/java/com/example/xcode/LoginActivity.java
+++ app/src/main/java/com/example/xcode/LoginActivity.java
@@ -7,25 +7,43 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button LoginButton;
+    private ImageView GoogleButton;
     private EditText UserEmail, UserPassword;
     private TextView NewAccountLink;
     private FirebaseAuth mAuth;
     private ProgressDialog loadingBar;
+    private static final int RC_SIGN_IN = 1;
+    private GoogleApiClient mGoogleSignInClient;
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -37,6 +55,7 @@ public class LoginActivity extends AppCompatActivity {
         UserEmail = (EditText) findViewById(R.id.login_email);
         UserPassword = (EditText) findViewById(R.id.login_password);
         LoginButton = (Button) findViewById(R.id.login_account);
+        GoogleButton = (ImageView)findViewById(R.id.google_signin_button);
         mAuth = FirebaseAuth.getInstance();
         loadingBar = new ProgressDialog(this);
 
@@ -58,7 +77,84 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
+        mGoogleSignInClient = new GoogleApiClient.Builder(this).enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                Toast.makeText(LoginActivity.this,"Google ile Giriş Başarısız, Hesaplarınızı Kontrol Edin...",Toast.LENGTH_SHORT).show();
+
+            }
+        })
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+
+        GoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
     }
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN)
+        {
+            loadingBar.setTitle("Google Hesabınızla Giriş Yapılıyor");
+            loadingBar.setMessage("Hesabınız Oluşturulurken Lütfen Bekleyiniz...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+            GoogleSignInResult result   =   Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+                loadingBar.dismiss();
+                Toast.makeText(LoginActivity.this,"Google Hesabınızla Giriş Yapılıyor...",Toast.LENGTH_SHORT).show();
+
+            }
+            else{
+                Toast.makeText(LoginActivity.this,"Erişim sağlanamadı...",Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+            }
+
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithCredential:success");
+                            loadingBar.dismiss();
+                            SendUserToMainActivity();
+                        } else {
+                            String message = task.getException().toString();
+                            loadingBar.dismiss();
+                            Toast.makeText(LoginActivity.this,"Hata Oluştu : "+ message,Toast.LENGTH_SHORT).show();
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            SendUserToLoginActivity();
+                        }
+
+                    }
+                });
+    }
+
 
     @Override
     protected void onStart()
@@ -87,8 +183,8 @@ public class LoginActivity extends AppCompatActivity {
             {
                 loadingBar.setTitle("Giriş Yapılıyor");
                 loadingBar.setMessage("Profiliniz Yükleniyor");
-                loadingBar.show();
                 loadingBar.setCanceledOnTouchOutside(true);
+                loadingBar.show();
                 mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -115,6 +211,13 @@ public class LoginActivity extends AppCompatActivity {
         Intent mainIntent = new Intent(LoginActivity.this , MainActivity.class);
         mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(mainIntent);
+        finish();
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(LoginActivity.this , LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
         finish();
     }
 
